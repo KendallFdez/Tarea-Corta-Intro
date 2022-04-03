591a93642d025572a6a3b1c973afad1b79f4e663
KendallFdez <102883424+KendallFdez@users.noreply.github.com>
2022-04-02 20:56:29 -0600

    V1

--- main.py
+++ main.py
@@ -12,6 +12,31 @@ from threading import Thread
 from tkinter import messagebox
 from timeit import default_timer
 
+#Funciones
+def mensaje():
+	answer=messagebox.askyesno("Salir", "¿Desea Salir?")
+	if(answer):
+		ventana.destroy()
+#fib:calcula el número n de la sucesión de Fibonacci
+#e:n
+#s:fib(n)
+#r:n es un entero positivo
+def fib():
+    num1=(cuadronum.get())
+    if isinstance(num1,int) and num1 >= 0:
+        return fib_aux(num1)
+    else:
+        return "Debe ser un número entero positivo"
+def fib_aux(num):
+    if num == 0 or num == 1: 
+        return numfib.set(fib)
+    else:
+        return fib_aux(num - 1) + fib_aux(num - 2)
+"""ifib=default_timer()
+fib()
+ffib=default_timer()
+tiempo=(ffib-ifib)"""
+
 #Colores
 negro="#4B4848"
 naranja="#C57C37"
@@ -31,42 +56,19 @@ panel_canvas.place(x=0, y=0)
 anim_canvas = tk.Canvas(ventana, width=466, height=556, borderwidth=0, highlightthickness=0, bg=negro)
 anim_canvas.place(x=236, y=0)
 
-#Funciones
-def mensaje():
-	answer=messagebox.askyesno("Salir", "¿Desea Salir?")
-	if(answer):
-		ventana.destroy()
-#fib:calcula el número n de la sucesión de Fibonacci
-#e:n
-#s:fib(n)
-#r:n es un entero positivo
-def fib(num):
-    if isinstance(num, int) and num >= 0:
-        return fib_aux(num)
-    else:
-        return "Debe ser un número entero positivo"
-def fib_aux(num):
-    if num == 0 or num == 1: 
-        return num
-    else:
-        return fib_aux(num - 1) + fib_aux(num - 2)
-numfib=tk.StringVar()
-num0=tk.Label(panel_canvas,text=numfib.get(),font=("Times",18),bg=verdeLima,fg=negro)
-def getNum():
-    num0.config(text=numfib.get())
-    num0.place(x=70,y=200)
-"""ifib=default_timer()
-fib()
-ffib=default_timer()
-tiempo=(ffib-ifib)"""
-
 #Entry
-cuadronum=tk.Entry(panel_canvas,textvariable=numfib)
+numfib=tk.StringVar()
+num0=tk.Label(panel_canvas,text="Inserte su número",font=("Times",18),bg=negro,fg="white")
+num0.place(x=25,y=135)  
+cuadronum=tk.Entry(panel_canvas)
 cuadronum.place(x=50,y=175)
+res=tk.Label(panel_canvas,textvariable=numfib,font=("Times",18),bg=negro,fg="white")
+res.place(x=25,y=210)
 
 #Botón
-boton=tk.Button(panel_canvas,text="Calcular",font=("fixedsys", "15"),bg=gris,command=getNum())
-boton.place(x=65, y=135)
+boton=tk.Button(panel_canvas,text="Calcular",font=("fixedsys", "15"),bg=gris,command=())
+boton.place(x=70, y=195)
+
 #Menu
 barraMenu=tk.Menu(ventana)
 barraMenu.add_command(label="Fibonacci",)
